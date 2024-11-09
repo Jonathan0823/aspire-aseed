63f90dd1530452b8bfd1fafe7e78cda74f7de79c
jefry-dot <sjefryip0519@gmail.com>
2024-11-09 23:03:08 +0700

    imporove

--- app/pages/Navbar.tsx
+++ app/pages/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar: React.FC = () => {
 
   return (
     <header className="absolute top-0 left-0 w-full flex justify-between items-center p-4 bg-opacity-50 bg-[#d2c6b7] backdrop-blur-lg z-10">
-      {/* Logo Section */}
+     
       <div className="flex items-center space-x-2 bg-[#d2c6b7] bg-opacity-80 rounded-full px-4 py-2">
         <FaUserCircle className="text-gray-800 w-5 h-5" />
-        <span className="text-gray-800 font-semibold">English Education Department</span>
+        <span className="text-[#040180] font-semibold">English Education Department</span>
       </div>
 
-      {/* Navigation Links */}
+      
       <nav className="hidden lg:flex space-x-4 border-2 border-purple-500 rounded-full bg-[#d2c6b7] bg-opacity-80 px-6 py-2">
         <a href="#" className="text-blue-900 font-bold hover:text-blue-600">HOME</a>
         <a href="#" className="text-blue-900 font-bold hover:text-blue-600">ABOUT</a>

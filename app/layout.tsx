e4dd798f1b47aebdb7691251113217e0da536889
Guinn <eguinjonathan08@gmail.com>
2024-11-10 06:22:25 +0700

    refactor Navbar styling and update font to Montserrat in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
+import { Montserrat } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const montserrat = Montserrat({ subsets: ['latin'] })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+
 
 export const metadata: Metadata = {
   title: "Aspire Aseed",
@@ -27,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${montserrat} antialiased`}>
         <Navbar />
         {children}
       </body>

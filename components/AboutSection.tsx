de7e1af9a958181e3f7b4695a58e5836eb4af293
Guinn <eguinjonathan08@gmail.com>
2024-11-10 09:12:44 +0700

    fix: adjust padding and margin in AboutSection and ContactSection for improved layout and responsiveness

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -8,7 +8,7 @@ const AboutSection = () => {
         <h2 className="bg-[#040180] border lg:mt-0 mt-20 border-white text-2xl text-white rounded-full py-2 px-20 inline-block font-bold mb-4">
           ABOUT
         </h2>
-      <div className="bg-[#d2c6b7] bg-opacity-90 rounded-lg p-8 md:max-w-xl mx-5 text-center">
+      <div className="bg-[#d2c6b7] bg-opacity-90 rounded-lg p-8 md:max-w-xl w-full px-4 text-center">
         <p className="text-[#040180] font-semibold text-xl leading-relaxed">
           AspireAseed is a website managed by the Association of Students of
           English Education Department. This website accommodates every student
--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -2,13 +2,14 @@ import Background from "@/components/background";
 
 const ContactSection = () => {
   return (
-    <div className="relative h-screen flex flex-col justify-center items-center">
+    <div className="relative h-screen flex flex-col justify-center items-center ">
       <Background />
+      <div className="flex flex-col justify-center items-center w-screen px-4">
 
         <h2 className="bg-[#040180] border border-white text-2xl text-white rounded-full py-2 px-20 inline-block font-bold mb-4">
           Contact
         </h2>
-      <div className="bg-[#d2c6b7] bg-opacity-90 rounded-lg p-8 md:max-w-xl w-full text-left mx-5 py-12 space-y-4">
+      <div className="bg-[#d2c6b7] bg-opacity-90 rounded-lg p-8 md:max-w-xl w-full text-left py-12 space-y-4">
         <p className="text-[#040180] font-semibold text-xl leading-relaxed">
           Email: 
         </p>
@@ -21,6 +22,7 @@ const ContactSection = () => {
         <p className="text-[#040180] font-semibold text-xl leading-relaxed">
           Contact Person: 
         </p>
+      </div>
       </div>
     </div>
   );

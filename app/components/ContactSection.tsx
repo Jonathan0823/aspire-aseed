a3a81b2d5af00dadfa4ff2d90e82c60ae2617fa6
Guinn <eguinjonathan08@gmail.com>
2024-11-10 07:20:22 +0700

    update ContactSection styling for improved spacing and readability

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -9,7 +9,7 @@ const ContactSection: React.FC = () => {
         <h2 className="bg-[#040180] border border-white text-2xl text-white rounded-full py-2 px-20 inline-block font-bold mb-4">
           ABOUT
         </h2>
-      <div className="bg-[#d2c6b7] bg-opacity-90 rounded-lg p-8 md:max-w-xl w-full text-left mx-5 ">
+      <div className="bg-[#d2c6b7] bg-opacity-90 rounded-lg p-8 md:max-w-xl w-full text-left mx-5 py-12 space-y-4">
         <p className="text-[#040180] font-semibold text-xl leading-relaxed">
           Email: 
         </p>

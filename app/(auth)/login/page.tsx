589b35f20587614b29bcb07296b30ab1506502cf
Guinn <eguinjonathan08@gmail.com>
2024-11-10 13:15:27 +0700

    feat: add login and register buttons with handlers to the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,22 +5,40 @@ import { useState } from 'react';
 const Page = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+
+  const handleLogin = async () => {
+    console.log(email, password)
+  }
+  const handleRegister = async () => {
+    console.log(email, password)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-screen md:pt-16 min-h-screen bg-cover bg-center relative">
       <Logo />
-      <div className=' text-white space-y-2 font-bold mt-5 max-w-lg'>
-        <div className='flex gap-2 bg-[#161f77] p-4 rounded-full px-5 border-2 border-orange-400'>
+      <div className=' text-white text-xl space-y-2 font-bold md:mt-5 mt-20 md:max-w-lg max-w-sm'>
+        <div className='flex gap-2 bg-[#161f77] p-4 rounded-full px-5 border-2 border-orange-400  justify-between w-full'>
           <p className='w-24'>Email</p><p>:</p>
           <input type="email" className='bg-[#161f77] focus:border-none focus:outline-none'
           onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className='flex gap-2 bg-[#161f77] p-4 rounded-full px-5 border-2 border-orange-400'>
+        <div className='flex gap-2 bg-[#161f77] p-4 rounded-full px-5 border-2 border-orange-400  justify-between w-full'>
           <p className='w-24'>Password</p><p>:</p>
           <input type="password" className='bg-[#161f77] focus:border-none focus:outline-none'
           onChange={(e) => setPassword(e.target.value)}
           />
         </div>
+        <div className='flex justify-center gap-3'>
+          <button className='bg-[#161f77] w-32 p-3 rounded-full px-5 border-2 border-orange-400'
+          onClick={handleLogin}
+          >Login</button>
+          <button className='bg-[#161f77] p-3 w-32 rounded-full px-5 border-2 border-orange-400'
+          onClick={handleRegister}
+          >Register</button>
+        </div>
+
       </div>
     </div>
   )

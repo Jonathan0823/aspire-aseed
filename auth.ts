674fd2e4133d449fb64cca8e103f46f1e65de476
Guinn <eguinjonathan08@gmail.com>
2024-11-10 14:08:00 +0700

    feat: integrate NextAuth with Prisma adapter for authentication

---
+++ auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { prisma } from "./lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {strategy: 'jwt'},
+  providers: [Credentials({
+    name: "Credentials",
+    credentials: {
+      email: { label: "Email", type: "text" },
+      password: { label: "Password", type: "password" },
+    },
+    authorize: async (credentials) => {
+      const password = credentials.password as string;
+      const email = credentials.email as string;
+      console.log(email, password);
+
+      if(!email || !password) {
+        return null;
+      }
+
+      const user = await prisma.user.findFirst({
+        where: {
+          email: email,
+        },
+      });
+      console.log(user);
+
+      if(!user || !user.password) {
+        return null;
+      }
+
+      const isValid = await bcrypt.compare(password, user.password);
+      if (!isValid) {
+        return null;
+      }
+      
+      return user;
+    }
+  })],
+});

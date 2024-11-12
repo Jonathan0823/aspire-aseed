9f0161447e650973e5457a7815c1e8d62a516375
Guinn <eguinjonathan08@gmail.com>
2024-11-12 11:13:08 +0700

    feat: add new pages for keamanan, akademik, kebersihan, and formsexxualhar with logo component

--- app/(aspirasi)/aspirasiTerbuka/page.tsx
+++ app/(aspirasi)/aspirasiTerbuka/page.tsx
@@ -1,6 +1,25 @@
 import Link from "next/link";
 
 const page = () => {
+    const menu = [
+        {
+            title: "Fasilitas Kampus",
+            link: "aspirasiterbuka/fasilitaskampus"
+        },
+        {
+            title: "Kebersihan",
+            link: "aspirasiterbuka/kebersihan"
+        },
+        {
+            title: "Keamanan",
+            link: "aspirasiterbuka/keamanan"
+        },
+        {
+            title: "Akademik",
+            link: "aspirasiterbuka/akademik"
+        }
+    ]
+
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen bg-cover bg-center relative">
             <div className="space-y-12 max-w-lg mx-auto flex flex-col mt-12 z-10 text-center py-11">
@@ -9,21 +28,15 @@ const page = () => {
                  </h3>
             </div>
 
-            <div className="bg-[#d2c6b7] p-6 sm:p-12 md:p-16 lg:p-20 xl:p-24 rounded-lg max-w-full sm:max-w-md md:max-w-lg lg:max-w-3xl w-full">
-                <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-8 text-center text-[#161f77] mt-6">Konteks Keluhan</h2>
-                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 gap-4 sm:gap-6 md:gap-8 mt-8">
-                    <Link href="/fasilitaskampus" className="bg-[#ececec] hover:bg-gray-300 text-[#161f77] font-bold py-4 px-6 sm:py-5 sm:px-7 md:py-6 md:px-8 text-sm sm:text-lg md:text-xl rounded-lg">
-                        Fasilitas Kampus
-                    </Link>
-                    <Link href="/kebersihan" className="bg-[#ececec] hover:bg-gray-300 text-[#161f77] font-bold py-4 px-6 sm:py-5 sm:px-7 md:py-6 md:px-8 text-sm sm:text-lg md:text-xl rounded-lg">
-                        Kebersihan
-                    </Link>
-                    <Link href="/keamanan" className="bg-[#ececec] hover:bg-gray-300 text-[#161f77] font-bold py-4 px-6 sm:py-5 sm:px-7 md:py-6 md:px-8 text-sm sm:text-lg md:text-xl rounded-lg">
-                        Keamanan
-                    </Link>
-                    <Link href="/akademik" className="bg-[#ececec] hover:bg-gray-300 text-[#161f77] font-bold py-4 px-6 sm:py-5 sm:px-7 md:py-6 md:px-8 text-sm sm:text-lg md:text-xl rounded-lg">
-                        Akademik
-                    </Link>
+            <div className="bg-[#d2c6b7] p-2 xl:p-24 rounded-3xl max-w-md md:max-w-lg lg:max-w-3xl w-full flex justify-center flex-col">
+                <h2 className="text-2xl md:text-4xl font-semibold mb-2 md:mb-8 text-center text-[#161f77] ">Konteks Keluhan</h2>
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-4 md:gap-20 mx-auto mt-2 pb-5">
+                    {menu.map((item, index) => (
+                        <Link key={index} href
+                        ={item.link} className="bg-[#ececec] hover:bg-gray-300 text-[#161f77] font-semibold py-2 px-1 md:w-auto w-44 md:py-2 md:px-8 text-center text-sm  md:text-2xl rounded-full">
+                            {item.title}
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>

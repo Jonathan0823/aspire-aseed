45c8e2a7cb2261d584f0464baaed0210bfd44b73
Guinn <eguinjonathan08@gmail.com>
2024-11-13 14:05:04 +0700

    Refactor FormTerbuka component: improve code readability and add handleSubmit function for form submission

--- components/FormTerbuka.tsx
+++ components/FormTerbuka.tsx
@@ -1,103 +1,115 @@
-"use client"
-import { useState } from 'react'
+"use client";
+import { useState } from "react";
 
-const FormTerbuka = ({type}: {type:string}) => {
-    const [nama, setNama] = useState("");
-    const [kelas, setKelas] = useState("");
-    const [angkatan, setAngkatan] = useState("");
-    const [keluhan, setKeluhan] = useState("");
-    const [bukti, setBukti] = useState("");
+const FormTerbuka = ({ type }: { type: string }) => {
+  const [nama, setNama] = useState("");
+  const [kelas, setKelas] = useState("");
+  const [angkatan, setAngkatan] = useState("");
+  const [keluhan, setKeluhan] = useState("");
+  const [bukti, setBukti] = useState("");
+
+  const handleSubmit = async () => {
+    console.log(nama, kelas, angkatan, keluhan, bukti);
+  };
 
   return (
-    <div><div className="flex flex-col justify-center items-center w-screen px-4 pt-20">
-    <h3 className="w-80 py-3 text-lg sm:text-xl md:text-2xl text-center mb-5 lg:text-3xl text-white font-bold rounded-full bg-[#273968] shadow-md hover:bg-[#1D2A56] border-2">
-      Aspirasi Terbuka
-    </h3>
+    <div>
+      <div className="flex flex-col justify-center items-center w-screen px-4 pt-20">
+        <h3 className="w-80 py-3 text-lg sm:text-xl md:text-2xl text-center mb-5 lg:text-3xl text-white font-bold rounded-full bg-[#273968] shadow-md hover:bg-[#1D2A56] border-2">
+          Aspirasi Terbuka
+        </h3>
 
-    <div className="bg-[#d2c6b7] bg-opacity-90 overflow-y-auto rounded-lg max-h-[500px] hide-scrollbar p-8 md:max-w-xl w-full text-left py-12 space-y-6">
-      <div className="text-[#161f77]">
-        <div className="bg-[#ececec] hover:bg-gray-300 font-bold px-4 py-2 text-sm sm:text-lg md:text-xl rounded-lg inline-block">
-          {type.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+        <div className="bg-[#d2c6b7] bg-opacity-90 overflow-y-auto rounded-lg max-h-[500px] hide-scrollbar p-8 md:max-w-xl w-full text-left py-12 space-y-6">
+          <div className="text-[#161f77]">
+            <div className="bg-[#ececec] hover:bg-gray-300 font-bold px-4 py-2 text-sm sm:text-lg md:text-xl rounded-lg inline-block">
+              {type
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+            </div>
+          </div>
+
+          <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
+            <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
+              <p className="w-24">Nama</p>
+              <p>:</p>
+              <input
+                type="text"
+                value={nama}
+                className=" w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
+                placeholder="Masukkan Nama"
+                onChange={(e) => setNama(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
+            <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
+              <p className="w-24">Kelas</p>
+              <p>:</p>
+              <input
+                type="text"
+                value={kelas}
+                className=" w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
+                placeholder="Masukkan Kelas"
+                onChange={(e) => setKelas(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
+            <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full ">
+              <p className="w-24">Angkatan</p>
+              <p>:</p>
+              <input
+                type="text"
+                value={angkatan}
+                className="border  w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
+                placeholder="Masukkan Angkatan"
+                onChange={(e) => setAngkatan(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
+            <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
+              <p className="w-24">Detail Keluhan</p>
+              <p>:</p>
+              <input
+                type="text"
+                value={keluhan}
+                className="border  w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
+                placeholder="Masukkan Angkatan"
+                onChange={(e) => setKeluhan(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
+            <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
+              <p className="w-24">Bukti Keluhan</p>
+              <p>:</p>
+              <input
+                type="file"
+                className=" w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
+                value={bukti}
+                onChange={(e) => setBukti(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-center mt-6">
+            <button
+              className="bg-[#040180] text-white py-2 px-6 rounded-lg hover:bg-[#030e64] focus:outline-none"
+              onClick={handleSubmit}
+            >
+              Kirim
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
-        <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
-          <p className="w-24">Nama</p>
-          <p>:</p>
-          <input
-            type="text"
-            value={nama}
-            className=" w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
-            placeholder="Masukkan Nama"
-            onChange={(e) => setNama(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
-        <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
-          <p className="w-24">Kelas</p>
-          <p>:</p>
-          <input
-            type="text"
-            value={kelas}
-            className=" w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
-            placeholder="Masukkan Kelas"
-            onChange={(e) => setKelas(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
-        <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full ">
-          <p className="w-24">Angkatan</p>
-          <p>:</p>
-          <input
-            type="text"
-            value={angkatan}
-            className="border  w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
-            placeholder="Masukkan Angkatan"
-            onChange={(e) => setAngkatan(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
-        <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
-          <p className="w-24">Detail Keluhan</p>
-          <p>:</p>
-          <input
-            type="text"
-            value={keluhan}
-            className="border  w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
-            placeholder="Masukkan Angkatan"
-            onChange={(e) => setKeluhan(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="text-[#161f77] text-xl space-y-2 font-bold md:mt-5 mt-5 w-full max-w-md">
-        <div className="flex gap-2 p-4 rounded-full px-5 justify-between w-full">
-          <p className="w-24">Bukti Keluhan</p>
-          <p>:</p>
-          <input
-            type="file"
-            className=" w-full sm:w-52 focus:outline-none px-3 py-2 rounded-full"
-            value={bukti}
-            onChange={(e) => setBukti(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="flex justify-center mt-6">
-        <button className="bg-[#040180] text-white py-2 px-6 rounded-lg hover:bg-[#030e64] focus:outline-none">
-          Kirim
-        </button>
       </div>
     </div>
-  </div></div>
-  )
-}
+  );
+};
 
-export default FormTerbuka
+export default FormTerbuka;
--- lib/action.ts
+++ lib/action.ts
@@ -1,57 +1,57 @@
-"use server"
-import bcrypt from 'bcryptjs';
-import { prisma } from './prisma';
-import { signIn, signOut } from '@/auth';
-import { revalidatePath } from 'next/cache';
+"use server";
+import bcrypt from "bcryptjs";
+import { prisma } from "./prisma";
+import { signIn, signOut } from "@/auth";
+import { revalidatePath } from "next/cache";
 
 export async function loginUser(email: string, password: string) {
   const user = await prisma.user.findUnique({
     where: {
-      email
-    }
+      email,
+    },
   });
   if (!user) {
-    throw new Error('User not found');
+    throw new Error("User not found");
   }
   const isValid = await bcrypt.compare(password, user.password);
   if (!isValid) {
-    throw new Error('Invalid password');
+    throw new Error("Invalid password");
   }
- 
-    await signIn('credentials', {email, password, redirectTo: '/'});
-    revalidatePath('/');
+
+  await signIn("credentials", { email, password, redirectTo: "/" });
+  revalidatePath("/");
 }
 
 export async function registerUser(email: string, password: string) {
   const user = await prisma.user.findUnique({
     where: {
-      email
-    }
+      email,
+    },
   });
   if (user) {
-    throw new Error('User already exists');
+    throw new Error("User already exists");
   }
-  try{
+  try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await prisma.user.create({
-        data: {
-            email,
-            password: hashedPassword
-        }
-        });
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
     return newUser;
   } catch (err) {
-      if (err instanceof Error) {
-        throw new Error(err.message);
-      } else {
-        throw new Error(String(err));
-      }
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error(String(err));
     }
+  }
 }
 
 export async function logoutUser() {
-  await signOut({redirectTo: '/login'});
-  revalidatePath('/login');
+  await signOut({ redirectTo: "/login" });
+  revalidatePath("/login");
 }
 
 export interface LaporanTerbuka {
@@ -60,6 +60,60 @@ export interface LaporanTerbuka {
   category: string;
 }
 
-export async function createLaporanTerbuka(){
-  // create laporan
+export async function createLaporanTerbuka(
+  userId: string,
+  nama: string,
+  kelas: string,
+  angkatan: string,
+  keluhan: string,
+  bukti: string,
+  type: string
+) {
+  try {
+    const laporan = await prisma.aspirasiTerbuka.create({
+      data: {
+        userId,
+        nama,
+        kelas,
+        angkatan,
+        keluhan,
+        buktiKeluhan: bukti,
+        type,
+      },
+    });
+    return laporan;
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error(String(err));
+    }
+  }
+}
+
+export async function createLaporanTertutup(
+  userId: string,
+  angkatan: string,
+  keluhan: string,
+  bukti: string,
+  type: string
+) {
+  try {
+    const laporan = await prisma.aspirasiTertutup.create({
+      data: {
+        userId,
+        angkatan,
+        keluhan,
+        buktiKeluhan: bukti,
+        type,
+      },
+    });
+    return laporan;
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error(String(err));
+    }
+  }
 }
